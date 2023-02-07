93fcb837600990a91be82aa348f9720ffea7fb9b
AbelEthan <abelethan@vip.qq.com>
2023-02-07 17:03:21 +0800

    feat(aspose): 使用工厂模式调用实现类
    1.添加工厂模式调用案例

--- aspose-demo/src/test/java/com/qxm/TestApplication.java
+++ aspose-demo/src/test/java/com/qxm/TestApplication.java
@@ -21,7 +21,7 @@ public class TestApplication {
     }
 
     @Test
-    public void testSlidesToPdf(){
+    public void testSlidesToPdf() {
         long startMillis = System.currentTimeMillis();
         AbstractFileConvert fileConvert = new SlidesFileConvert();
         String pdfFilePath = fileConvert.getResultPath("C:\\Users\\AbelEthan\\IdeaProjects\\code-demo\\aspose-demo\\src\\main\\resources\\testPpt.pptx");
@@ -30,12 +30,28 @@ public class TestApplication {
     }
 
     @Test
-    public void testCellsToPdf(){
+    public void testCellsToPdf() {
         long startMillis = System.currentTimeMillis();
         AbstractFileConvert fileConvert = new CellsFileConvert();
         String pdfFilePath = fileConvert.getResultPath("C:\\Users\\AbelEthan\\IdeaProjects\\code-demo\\aspose-demo\\src\\main\\resources\\testExcel.xlsx");
         System.out.println(System.currentTimeMillis() - startMillis);
         System.out.println(pdfFilePath);
+    }
+
+    @Test
+    public void testFileConvertEnum() {
+        long wordsStartMillis = System.currentTimeMillis();
+        AbstractFileConvert wordsFileConvert = FileConvertEnum.getFileConvert("WORD");
+        wordsFileConvert.getResultPath("C:\\Users\\AbelEthan\\IdeaProjects\\code-demo\\aspose-demo\\src\\main\\resources\\testWord.docx");
+        System.out.println("WORD转换时间：" + (System.currentTimeMillis() - wordsStartMillis));
+        long slidesStartMillis = System.currentTimeMillis();
+        AbstractFileConvert slidesFileConvert = FileConvertEnum.getFileConvert("PPT");
+        slidesFileConvert.getResultPath("C:\\Users\\AbelEthan\\IdeaProjects\\code-demo\\aspose-demo\\src\\main\\resources\\testPpt.pptx");
+        System.out.println("PPT转换时间：" + (System.currentTimeMillis() - slidesStartMillis));
+        long cellsStartMillis = System.currentTimeMillis();
+        AbstractFileConvert cellsFileConvert = FileConvertEnum.getFileConvert("EXCEL");
+        cellsFileConvert.getResultPath("C:\\Users\\AbelEthan\\IdeaProjects\\code-demo\\aspose-demo\\src\\main\\resources\\testExcel.xlsx");
+        System.out.println("EXCEL转换时间：" + (System.currentTimeMillis() - cellsStartMillis));
     }
 
 
